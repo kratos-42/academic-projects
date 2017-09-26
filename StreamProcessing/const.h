49d841a847f3b31828cbbdf15c29f437ffc4c31e
Daniel Rodrigues <danielg.rodrigues42@gmail.com>
2017-09-26 17:29:20 +0100

    \.../

---
+++ StreamProcessing/const.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+
+#define MAX_SIZE 1024
+
+
+
+
+
+
+
+void addColumn(char* args);
---
+++ StreamProcessing/controlador.h
@@ -0,0 +1,24 @@
+#include <string.h>
+#include <unistd.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include "utils.h"
+#include "uthash.h"
+
+#define MAX_SIZE 1024
+#define MAX_NODES 100
+
+struct node {
+	int id;
+	char** cmd;
+	int nrConnections;
+	int pd[2];
+	int running;
+	int connected_nodes[MAX_NODES];
+	UT_hash_handle hh;
+};
+
+struct node* rede = NULL;
+
