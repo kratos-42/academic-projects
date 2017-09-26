c95741d71dd7422043c12ac49309507f442e3fbf
Daniel Rodrigues <danielg.rodrigues42@gmail.com>
2017-09-26 17:34:55 +0100

    garbage deleted

--- StreamProcessing/utils.h
+++
@@ -1,9 +0,0 @@
-#include <string.h>
-#include <unistd.h>
-#include <assert.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-ssize_t readln(int fd, void* buf, size_t bsize);
-char** split(char* string, char* sep);
-int getCollumn(char* str, int index, char* collumn, int n);
--- window.h
+++
@@ -1,7 +0,0 @@
-#include <string.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-
-#define MAX_SIZE 1024
